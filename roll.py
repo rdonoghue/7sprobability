58fa4c54507272dd12ad3e609ec0e8a75b48ec30
rdonoghue <rdonoghue@gmail.com>
2016-12-16 22:13:25 -0500

    Ok, got duos and trios up and running.  Cleaning the remainder is going to require some compression and willingness to get wrong #'s

--- roll.py
+++ roll.py
@@ -21,21 +21,39 @@ def pairs(rollset, raises):
     setsize=len(rollset)
     for x in range (0, setsize):
         for y in range (0,setsize):
-            print "%r, %r" % (rollset[x], rollset[y]),
-            if x < y:
+#            print "%r, %r" % (rollset[x], rollset[y]),
+            if x != y:
                 if rollset[x] + rollset[y] == 10:
                     raises += 1
-                    rollset.pop(y)
-                    rollset.pop(x)
-                    setsize = setsize - 2
-            elif x > y:
-                if rollset[x] + rollset[y] == 10:
-                    raises += 1
-                    rollset.pop(x)
-                    rollset.pop(y)
-                    setsize = setsize - 2
-#    return raises, rollset
+                    rollset[y]=0
+                    rollset[x]=0
+    return raises, rollset
 # Note to self - when it comes time to tighten the stack, check if 0 + 1 < 10
+
+def trios(rollset, raises):
+    setsize=len(rollset)
+    for x in range (0, setsize):
+        for y in range (0,setsize):
+            for z in range (0,setsize):
+                if (x!= y and x!= z and y!=z):
+                    if rollset[x] + rollset[y] + rollset[z] == 10:
+                        raises += 1
+                        rollset[y]=0
+                        rollset[x]=0
+                        rollset[z]=0
+    return raises, rollset
+
+
+def cleanzeroes(rollset):
+    # This is probably uncessary, but leaving the step in for now to be clean
+    while 0 in rollset:
+        for x in range (0, len(rollset)):
+            if rollset[x] == 0:
+                rollset.pop(x)
+                break
+    return rollset
+
+
 
 
 dicerolled = 10
@@ -45,12 +63,21 @@ raises = 0
 
 diceroll = sevenroll(dicerolled)
 print "Initial: %r" % (diceroll)
+
 result1, diceleft =tens(diceroll)
+print "Tens: %r: %r" % (result1, diceleft)
 
-print "Tens: %r: %r" %(result1, diceleft)
-pairs(diceleft, result1)
-print "Pairs: "
+result2, diceleft = pairs(diceleft, result1)
+print "Pairs: %r: %r" % (result2, diceleft)
 
+diceleft = cleanzeroes(diceleft)
+print "Cleaned: %r: %r" % (result2, diceleft)
+
+result3, diceleft = trios(diceleft, result2)
+print "Trios: %r: %r" % (result3, diceleft)
+
+diceleft = cleanzeroes(diceleft)
+print "Cleaned: %r: %r" % (result3, diceleft)
 
 
 
